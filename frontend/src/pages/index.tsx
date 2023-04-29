77b7829a39a5d5e38494c2e073f93796c1352e98
jedeop <jedeop@jedeop.dev>
2023-04-29 19:36:16 +0900

    frontend: add notice at `/`

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -37,6 +37,16 @@ export default function Home() {
           <span className="text-red-400">*</span>현재 베타 테스트 중입니다.
           혹시 오류가 발생한다면 <a className="text-blue-600" href="https://github.com/jedeop/holssi-website/issues">이곳</a>에 이슈를 생성해주세요.
         </div>
+        <br />
+        <div>
+          <span className="text-red-400">*</span>
+          업로드한 엔트리 작품 파일 및 빌드된 실행 파일은 최대 48시간 이후 서버에서 삭제됩니다.
+        </div>
+        <div>
+          <span className="text-red-400">*</span>
+          서비스 제공, 개선 및 연구 등을 위해 다음과 같은 정보들이 자동으로 생성되어 수집될 수 있습니다. <br />
+          - IP 주소, 쿠키, 방문 일시, 서비스 이용 기록(사용자가 입력한 작품 정보 포함), 불량 이용 기록
+        </div>
       </main>
     </>
   )
