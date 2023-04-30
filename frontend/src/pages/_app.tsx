e5aaf6c50b1947340bfcd1868f55c01453e34d87
jedeop <jedeop@jedeop.dev>
2023-04-30 21:18:30 +0900

    frontend: Vercel Web Analytics -> Google Analytics

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,12 +1,45 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Analytics } from '@vercel/analytics/react'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import * as gtag from '../utils/gtag'
+import Head from 'next/head'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
+        {/* Google Analytics https://github.com/vercel/next.js/tree/canary/examples/with-google-analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Component {...pageProps} />
-      <Analytics />
     </>
   )
 }
